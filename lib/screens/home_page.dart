d7a15628a729f75948f8dfa4b20d69e267399ba6
m.lakhani.24471 <m.lakhani.24471@khi.iba.edu.pk>
2024-05-27 06:39:37 +0000

    Initial commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Text"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
